b391e6224fca2331521768635f4589c0c2729e1f
shunshun66 <x_wind@163.com>
2018-09-07 17:27:08 +0800

    增加发货页面

--- pages/goodsedit/goodsedit.js
+++ pages/goodsedit/goodsedit.js
@@ -1,18 +1,67 @@
 // pages/goodsedit/goodsedit.js
+const util = require('../../utils/util.js');
+const api = require('../../config/config.js');
+const { $Message } = require('../../dist/base/index');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    form: {
+      goodsName:'',
+      prices:0,
+      amount:0,
+      reciveOpenid:'',
+      sendDate:''
+    }
   },
-
+  bindDateChange: function (e) {
+    const inspection_date =  e.detail.value
+    this.setData({
+      'form.sendDate': inspection_date
+    })
+  },
+  initForm: function () {
+    const inspection_date = util.dateformat(new Date(), 'yyyy-MM-dd');
+    this.setData({
+      'form.sendDate': inspection_date
+    })
+  },
+  formSubmit: function (e) {
+    this.setData({
+      form: e.detail.value
+    })
+    util.request(api.Business, this.data.form, "POST")
+    .then(res => {
+      if (!res.data.err) {
+        $Message({
+          content: '发货成功',
+          type: 'success'
+        })
+      } else {
+        $Message({
+          content: '发货失败',
+          type: 'error'
+        })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      $Message({
+        content: '发货失败',
+        type: 'error'
+      })
+    })
+  },
+  formReset: function () {
+    this.initForm()
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.initForm();
   },
 
   /**
--- utils/util.js
+++ utils/util.js
@@ -15,6 +15,27 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const dateformat = (strdate, fmt) => {
+  const self = new Date(strdate)
+  var o = {
+      'M+': self.getMonth() + 1, // 月份
+      'd+': self.getDate(), // 日
+      'h+': self.getHours(), // 小时
+      'm+': self.getMinutes(), // 分
+      's+': self.getSeconds(), // 秒
+      'q+': Math.floor((self.getMonth() + 3) / 3), // 季度
+      'S': self.getMilliseconds() // 毫秒
+  };
+  if (/(y+)/.test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, (self.getFullYear() + '').substr(4 - RegExp.$1.length));
+  }
+  for (var k in o) {
+      if (new RegExp('(' + k + ')').test(fmt)) {
+          fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+      }
+  }
+  return fmt;
+}
 /**
  * 封封微信的的request
  */
@@ -182,6 +203,7 @@ function wanlaiLogin() {
 
 module.exports = {
   formatTime: formatTime,
+  dateformat,
   request,
   showErrorToast,
   checkSession,
